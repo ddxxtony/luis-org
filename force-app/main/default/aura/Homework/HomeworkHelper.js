258ad2c44c74e648aa9414042335d9d136600038
Luis -Aguilar <luis.aguilar9503@hotmail.com>
2021-11-15 23:24:37 -0600

    + not sure

--- force-app/main/default/aura/Homework/HomeworkHelper.js
+++ force-app/main/default/aura/Homework/HomeworkHelper.js
@@ -2,20 +2,7 @@
     getAcc: function(component) {
         component.set("v.spinnerRender", true);
         // Create the action
-        let action = component.get("c.getAccounts");
-        // Add callback behavior for when response is received
-        action.setCallback(this, function(response) {
-            let state = response.getState();
-            if (state === "SUCCESS") {
-                let list = response.getReturnValue().map((a) => ({ label: a.Name, value: a.Id }));
-                component.set("v.account", list);
-
-            } else {
-                console.log("Failed with state: " + state);
-            }
-        });
-        // Send action off to be executed
-        $A.enqueueAction(action);
+        
 
         component.set("v.actionName", '');
         setTimeout(function() {
